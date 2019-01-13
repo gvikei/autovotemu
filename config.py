3012d918e881d4da934e21e2d26197ea417ad4cb
vudung45 <vudung45@yahoo.com>
2019-01-13 13:42:02 +0700

    first commit

---
+++ config.py
@@ -0,0 +1,11 @@
+class CONFIG:
+	class SELL_ITEM:
+		username = "squiral03"
+		password = "tuanphong"
+		char_name = "magigaladi"
+		price = 40
+		start_slot = 42
+		num_sell = 4
+		buy_many = False
+		auto_sell = False
+		use_proxy = True
