0fc2cfa7d3de748d9ec96f87afee44ca3396f86c
vudung45 <vudung45@yahoo.com>
2019-01-15 21:33:00 +0700

    update code

--- config.py
+++ config.py
@@ -1,21 +1,23 @@
 class CONFIG:
 	class SELL_ITEM:
-		username = ""
-		password = ""
+		username = "davidvu4"
+		password = "Dotkich123"
 		char_name = "DarkMG"
 		price = 79
 		start_slot = 98
 		num_sell = 5
 		turns = 1
 		buy_many = False
-		auto_sell = False
+		auto_sell = True
 		sell_proxy = False
 		login_proxy = False
+		sleep_time = 3
 	class VOTE:
 		login_proxy = False
 
 	class GET_BALANCE:
-		login_proxy = False
+		login_proxy = True
+		sleep_time = 5
 # usd15
 # 4ae242
 # usd006
